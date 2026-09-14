2d7422622287f7f3f5c11762b897154786221b6a
benedict <benedictiknkeonye@gmail.com>
2026-09-14 17:54:29 -0400

    feat: added and configured package for drag and drop functionality

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <DndProvider backend={HTML5Backend}>
+      <RouterProvider router={router} />
+    </DndProvider>
   </StrictMode>,
 );
