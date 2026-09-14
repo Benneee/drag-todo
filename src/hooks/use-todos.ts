c816b79f771b2259082c13160c29bf4638c07f0b
benedict <benedictiknkeonye@gmail.com>
2026-09-14 17:54:54 -0400

    feat: add new hook to control status update

--- src/hooks/use-todos.ts
+++ src/hooks/use-todos.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-import { ToDo } from "../types";
+import { ToDo, ToDoStatus } from "../types";
 import { TODOS_KEY } from "../constants";
 
 interface TodosState {
@@ -9,6 +9,7 @@ interface TodosState {
   deleteTodo: (todo: ToDo) => void;
   deleteAllTodos: () => void;
   markToDoAsDone: (todo: ToDo) => void;
+  updateToDoStatus: (newStatus: ToDoStatus, todo: ToDo) => void;
 }
 
 // create the todos store with local storage
@@ -45,6 +46,14 @@ export const useTodosStore = create<TodosState>()(
         set({ todos: updatedToDos });
         return updatedToDos;
       },
+      updateToDoStatus: (newStatus: ToDoStatus, todo: ToDo) => {
+        const existingToDos: ToDo[] = get().todos;
+        const updatedToDos: ToDo[] = existingToDos.map((item: ToDo) =>
+          item.id === todo.id ? { ...item, status: newStatus } : item,
+        );
+        set({ todos: updatedToDos });
+        return updatedToDos;
+      },
     }),
     {
       name: TODOS_KEY,
@@ -71,4 +80,9 @@ export function useMarkAsDone() {
 export function useClearToDos() {
   const clearToDos = useTodosStore((state) => state.deleteAllTodos);
   return () => clearToDos();
+}
+
+export function useUpdateToDoStatus() {
+  const updateToDoStatus = useTodosStore((state) => state.updateToDoStatus);
+  return (newStatus: ToDoStatus, todo: ToDo) => updateToDoStatus(newStatus, todo);
 }
